266f6c2199984d5b0f88ca0989f614586d989f1e
codeyoung <77968955+sjyoung123@users.noreply.github.com>
2022-01-08 19:51:13 +0900

    Update easy get

--- text.py
+++ text.py
@@ -1,3 +1,37 @@
+# import requests
+# from bs4 import BeautifulSoup
+
+# LIMIT = 50
+
+# BASE_URL = f"https://kr.indeed.com/취업?as_and=python&limit={LIMIT}"
+
+# item_list = []
+
+
+# def get_indeed_last_page():
+#     index = 100
+#     go = True
+
+#     while go:
+#         indeed_result = requests.get(f"{BASE_URL}&start={index*LIMIT}")
+#         indeed_soup = BeautifulSoup(indeed_result.text,"html.parser")
+        
+#         ul = indeed_soup.find("ul",{"class":"pagination-list"})
+       
+#         lis = ul.find_all("li")
+#         index +=1
+        
+#         for li in lis:
+#             if(not ul.find("svg")):
+#                 go = False
+#                 item_list.append(li)
+#                 return
+#             item_list.append(li)
+           
+
+# get_indeed_last_page()
+# print(item_list[-1])
+
 import requests
 from bs4 import BeautifulSoup
 
@@ -5,29 +39,13 @@ LIMIT = 50
 
 BASE_URL = f"https://kr.indeed.com/취업?as_and=python&limit={LIMIT}"
 
-item_list = []
 
+indeed_result = requests.get(f"{BASE_URL}&start=9999")
 
-def get_indeed_last_page():
-    index = 100
-    go = True
+indeed_soup = BeautifulSoup(indeed_result.text,"html.parser")
 
-    while go:
-        indeed_result = requests.get(f"{BASE_URL}&start={index*LIMIT}")
-        indeed_soup = BeautifulSoup(indeed_result.text,"html.parser")
-        
-        ul = indeed_soup.find("ul",{"class":"pagination-list"})
-       
-        lis = ul.find_all("li")
-        index +=1
-        
-        for li in lis:
-            if(not ul.find("svg")):
-                go = False
-                item_list.append(li)
-                return
-            item_list.append(li)
-           
+ul = indeed_soup.find("ul",{"class":"pagination-list"})
 
-get_indeed_last_page()
-print(item_list[-1])
+lis = ul.find_all("li")
+
+print(lis[-1])
